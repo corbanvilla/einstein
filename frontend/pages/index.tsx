83f090079a95420b7c36dcb5a596ad61520f7b5c
Pauline Wee <paulinekowee@gmail.com>
2022-11-13 12:53:33 +0400

    frontend
    draggable list

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,19 +3,102 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Draggable from 'react-draggable';
+import React from 'react'
+import { PERMANENT_REDIRECT_STATUS } from 'next/dist/shared/lib/constants';
+
+type DraggableEventHandler = (e: Event, data: DraggableData) => void | false;
+type DraggableData = {
+  node: HTMLElement,
+  // lastX + deltaX === x
+  x: number, y: number,
+  deltaX: number, deltaY: number,
+  lastX: number, lastY: number
+};
+
+// const displayData = (i:any, data:any) => {
+//   console.log(i);
+//   console.log(data);
+// }
+
+const printCoords = (i:any, data:any) => {
+  console.log(i);
+  console.log(data);
+}
+ 
 const Home: NextPage = () => {
+
+  const classes = [
+    {
+      id: 'CS-1001',
+      name: 'Intro to CS',
+      credits: 4,
+      des: 'The intro class bro'
+    },
+
+    {
+      id: 'CS-1002',
+      name: 'Discrete Math',
+      credits: 4,
+      des: 'The second intro class bro'
+    },
+
+    {
+      id: 'CS-1221',
+      name: 'Data Structures',
+      credits: 4,
+      des: 'The third intro class bro'
+    }
+
+  ]
+
+  function handleClick(e) {
+    console.log('click');
+    e.target.style.backgroundColor = 'blue';
+  }
+  
+
   return (
     <div className="container max-w-full w-full h-full max-h-full">
 
-      
-    
       <div className="flex items-stretch bg-grey-lighter min-h-screen">
 
-        <div className="flex-none w-[30%] bg-sky-400">
+        {/*sidebar - scrollable*/}
+        <div className="flex-none p-6 w-[20%] bg-blue-300">
 
-        hello world
+        <h2 className="text-2xl font-semibold">Classes</h2>
+
+          {/* <div className="flex-none p-2 pl-4 w-[100%] bg-sky-100 rounded-2xl">
+            <p className="text-black-700">Add class here</p>
+          </div> */}
+
+          <ul>
+            {classes.map(({id, name, des}) => (
+              <li key={id} className="z-5">
+                <Draggable
+                  onDrag={printCoords}
+                  grid={[360, 500]}
+                >
+                  <div className="flex-none p-3 pl-4 mt-4 w-[100%] bg-sky-100 rounded">
+                    <p className="text-xs">{id}</p>
+                    <h3 className="text-l font-semibold" key={name}>{name}</h3>
+                    <p>{des}</p>
+                  </div>
+                </Draggable>
+              </li>
+            ))}
+          </ul>
+
         </div>
-        <div className="flex-1">else here</div>
+        
+        {/*sidebar2*/}
+        <div className="flex-none bg-red-200 fh-screen sticky p-6 w-[20%] z-0">
+          <h2 className="text-2xl font-semibold">My Schedule</h2>
+          <p className="">Credits: <span className="creditSum">0</span></p>
+        </div>
+
+        {/*content - sticky*/}
+        <div className="fh-screen sticky top-0"></div>
 
       </div>
 
