af5483658e56118cac6787cabeab7758fd2d2cdd
corbanvilla <corbanvilla@gmail.com>
2022-11-13 14:58:29 +0400

    halas

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -112,7 +112,7 @@ export default function Home({ classes }: InferGetStaticPropsType<typeof getServ
                         <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                             {classData?.name}
                         </h3>
-                        <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                        <button type="button" onClick={handleClose} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
                             <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             <span className="sr-only">Close modal</span>
                         </button>
@@ -209,7 +209,7 @@ export default function Home({ classes }: InferGetStaticPropsType<typeof getServ
       
       {/*content - sticky*/}
        <div className="sticky bottom-0 right-0 h-[20%] h-min-[800px] p-6 ml-[288px] bg-white z-50">
-          <h2 className="text-xl font-semibold mb-2">Your Goal: Computer Science Major, Interactive Media Minor</h2>
+          <h2 className="text-xl font-semibold mb-2">Your Goal: Computer Science Major</h2>
 
           <div className="grid grid-cols-4">
 
