3008135b7a2ef2e2737cdb2469eda7b120b59dc2
corbanvilla <corbanvilla@gmail.com>
2022-11-13 14:44:24 +0400

    updates

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -48,12 +48,24 @@ export default function Home({ classes }: InferGetStaticPropsType<typeof getServ
   const [creditSum2, setcreditSum2] = useState(0);
   const [creditSum3, setcreditSum3] = useState(0);
   const [creditSum4, setcreditSum4] = useState(0);
+   console.log(classes);
+
+  const [show, setShow] = useState(false);
+  const [classData, setClassData] = useState({});
+  const [height, setHeight] = useState(0);
+
+  const handleClose = () => setShow(false);
+  const handleShow = (name: string, des: string, timings: string[]) => {
+    setClassData({name, des, timings});
+    setShow(true)
+  };
 
   //@ts-ignore
   function handleClick(e) {
     window.screen.height
     console.log('click');
-    e.target.style.backgroundColor = 'blue';
+    console.log(window.screen.height)
+    // e.target.style.backgroundColor = 'blue';
   }
 
 
@@ -78,11 +90,58 @@ export default function Home({ classes }: InferGetStaticPropsType<typeof getServ
     //column 4
     if (data.x>=col*4 && data.x<col*5) {setcreditSum4(creditSum4-4);}
   }
+  
+  useEffect(() => {
+    if (window) { 
+      //@ts-ignore
+      setHeight(window.screen.height);
+      console.log(`height: ${height}`);
+    }
+  });
 
   return (
 
     <div className="container max-w-full w-full h-full max-h-full">
 
+      {/* Modal */}
+      {/* <div className="absolute top-[50%] left-[50%]"> */}
+        <div hidden={!show} id="defaultModal" aria-hidden="true" className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-[100] w-full md:inset-0 h-modal md:h-full">
+            <div className="absolute top-[50%] left-[50%] p-4 w-full max-w-3xl h-full md:h-auto translate-x-[-50%] translate-y-[-50%]">
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                            {classData?.name}
+                        </h3>
+                        <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                            <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            <span className="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    <div className="p-6 space-y-6">
+                        <p className="text-base leading-relaxed dark:text-gray-400 text-black">
+                            {classData?.des}
+                            <br></br><br></br>
+                        </p>
+                        {/* <tbody> */}
+                          {/*@ts-ignore*/}
+
+
+                          {classData?.timings?.map((timing: string) => (
+                            <tr key={timing}>
+                              <td className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                {timing}
+                              </td>
+                            </tr>
+                            ))}
+                        {/* </tbody> */}
+                    </div>
+                    <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                        <button onClick={handleClose} data-modal-toggle="defaultModal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ok</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      {/* </div> */}
       <div className="flex items-stretch bg-grey-lighter min-h-screen">
 
         {/*sidebar - scrollable*/}
@@ -90,9 +149,13 @@ export default function Home({ classes }: InferGetStaticPropsType<typeof getServ
 
           <h2 className="text-2xl font-semibold bg-gray-200 sticky top-0 p-6 pt-4 pb-4 z-20">Classes</h2>
 
-          <ul className="pl-6 pr-6 flex-wrap">
-            {classes.map(({id, name, des}) => (
-              <li key={id}>
+          {/* <div className="flex-none p-2 pl-4 w-[100%] bg-sky-100 rounded-2xl">
+            <p className="text-black-700">Add class here</p>
+          </div> */}
+
+          <ul>
+            {classes.map(({id, name, des, timings}) => (
+              <li onClick={() => handleShow(name, des, timings)} key={id} className="z-5">
                 <Draggable
                   onDrag={printCoords}
                   grid={[289, 20]}
