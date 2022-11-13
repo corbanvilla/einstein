f37aa3c188b8933da2423e668d34f3dfabbd2f99
Pauline Wee <paulinekowee@gmail.com>
2022-11-13 14:43:14 +0400

    more stuff

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css'
 import { InferGetStaticPropsType } from 'next'
 import { CSData } from './cs';
@@ -27,22 +27,10 @@ interface ClassData {
   credits: number; 
 }
 
-// const displayData = (i:any, data:any) => {
-//   console.log(i);
-//   console.log(data);
-// }
-
-let creditSum = 0;
+const col = 289;
 
 const printCoords = (i:any, data:any) => {
-  //console.log(i);
   console.log(data);
-
-  if (data.x>289 && data.x<575) { //first column
-
-  }
-
-  //console.log(data.node);
 }
 
 export function getServerSideProps(context: any) {
@@ -52,20 +40,43 @@ export function getServerSideProps(context: any) {
       classes: CSData as ClassData[]
     }
   }
-
 } 
-
-
  
 export default function Home({ classes }: InferGetStaticPropsType<typeof getServerSideProps>) {
 
-   console.log(classes);
+  const [creditSum1, setcreditSum1] = useState(0);
+  const [creditSum2, setcreditSum2] = useState(0);
+  const [creditSum3, setcreditSum3] = useState(0);
+  const [creditSum4, setcreditSum4] = useState(0);
 
   //@ts-ignore
   function handleClick(e) {
     window.screen.height
     console.log('click');
     e.target.style.backgroundColor = 'blue';
+  }
+
+
+  const addCredits = (i:any, data:any) => {
+    //column 1
+    if (data.x>=col && data.x<col*2) {setcreditSum1(creditSum1+4);}
+    //column 2
+    if (data.x>=col*2 && data.x<col*3) {setcreditSum2(creditSum2+4);}
+    //column 3
+    if (data.x>=col*3 && data.x<col*4) {setcreditSum3(creditSum3+4);}
+    //column 4
+    if (data.x>=col*4 && data.x<col*5) {setcreditSum4(creditSum4+4);}
+  }
+
+  const removeCredits = (i:any, data:any) => {
+    //column 1
+    if (data.x>=col && data.x<col*2) {setcreditSum1(creditSum1-4);}
+    //column 2
+    if (data.x>=col*2 && data.x<col*3) {setcreditSum2(creditSum2-4);}
+    //column 3
+    if (data.x>=col*3 && data.x<col*4) {setcreditSum3(creditSum3-4);}
+    //column 4
+    if (data.x>=col*4 && data.x<col*5) {setcreditSum4(creditSum4-4);}
   }
 
   return (
@@ -85,8 +96,10 @@ export default function Home({ classes }: InferGetStaticPropsType<typeof getServ
                 <Draggable
                   onDrag={printCoords}
                   grid={[289, 20]}
+                  onStart={removeCredits}
+                  onStop={addCredits}
                 >
-                  <div className="flex-none p-3 pl-4 mt-4 w-[100%] bg-white rounded">
+                  <div className="flex-none p-3 pl-4 mt-4 mb-4 w-[100%] bg-white rounded">
                     <p className="text-xs">{id}</p>
                     <h3 className="text-l font-semibold" key={name}>{name}</h3>
                     {/* <p>{des.slice(0,80)}...</p> */}
@@ -102,7 +115,7 @@ export default function Home({ classes }: InferGetStaticPropsType<typeof getServ
         <div className="flex-none bg-red-200 fh-screen w-[20%]">
           <div className="bg-red-200 p-6 pt-4 pb-3 sticky top-0 z-20">
             <h2 className="text-2xl font-semibold">Freshman Year</h2>
-            <p className="">Credits: <span className="creditSum">0</span></p>
+            <p className="">Credits: {creditSum1}</p>
           </div>
         </div>
         
@@ -110,7 +123,7 @@ export default function Home({ classes }: InferGetStaticPropsType<typeof getServ
         <div className="flex-none bg-blue-200 fh-screen w-[20%]">
           <div className="bg-blue-200 p-6 pt-4 pb-3 sticky top-0 z-20">
             <h2 className="text-2xl font-semibold">Sophomore Year</h2>
-            <p className="">Credits: <span className="creditSum">0</span></p>
+            <p className="">Credits: {creditSum2}</p>
           </div>
         </div>
 
@@ -118,21 +131,58 @@ export default function Home({ classes }: InferGetStaticPropsType<typeof getServ
         <div className="flex-none bg-yellow-200 fh-screen w-[20%]">
           <div className="bg-yellow-200 p-6 pt-4 pb-3 sticky top-0 z-20">
             <h2 className="text-2xl font-semibold">Junior Year</h2>
-            <p className="">Credits: <span className="creditSum">0</span></p>
+            <p className="">Credits: {creditSum3}</p>
           </div>
         </div>
 
         <div className="flex-none bg-green-200 fh-screen w-[20%]">
           <div className="bg-green-200 p-6 pt-4 pb-3 sticky top-0 z-20">
             <h2 className="text-2xl font-semibold">Senior Year</h2>
-            <p className="">Credits: <span className="creditSum">0</span></p>
+            <p className="">Credits: {creditSum4}</p>
           </div>
         </div>
 
-        {/*content - sticky*/}
-        <div className="fh-screen sticky top-0"></div>
-
       </div>
+      
+      {/*content - sticky*/}
+       <div className="sticky bottom-0 right-0 h-[20%] h-min-[800px] p-6 ml-[288px] bg-white z-50">
+          <h2 className="text-xl font-semibold mb-2">Requirements</h2>
+
+          <div className="grid grid-cols-5">
+
+            <div className="col-span-1 col-start-1">
+                <h2 className="text-l font-semibold mb-2">CS Major</h2>
+                <input type="checkbox"/>
+                <label> Class 1</label><br/>
+                <input type="checkbox"/>
+                <label> Class 2</label><br/>
+                <input type="checkbox"/>
+                <label> Class 3</label><br/>
+                <input type="checkbox"/>
+                <label> Class 4</label><br/>
+                <input type="checkbox"/>
+                <label> Class 5</label><br/>
+            </div>
+
+            <div className="col-span-1 col-start-2">
+                <h2 className="text-l font-semibold mb-2">CS Major</h2>
+                <input type="checkbox"/>
+                <label> Class A</label><br/>
+                <input type="checkbox"/>
+                <label> Class 2</label><br/>
+                <input type="checkbox"/>
+                <label> Class 3</label><br/>
+                <input type="checkbox"/>
+                <label> Class 4</label><br/>
+                <input type="checkbox"/>
+                <label> Class 5</label><br/>
+            </div>
+
+
+          </div>
+
+          
+        </div>
 
     </div>
   )
