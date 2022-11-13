d4b9ba78049a7d16515489de1d3b26a798579cd2
corbanvilla <corbanvilla@gmail.com>
2022-11-13 10:17:05 +0400

    parser

--- backend/parser.py
+++ backend/parser.py
@@ -13,10 +13,27 @@ html_soup.prettify()
 
 classes = html_soup.find_all(['table'])
 
+all_classes = set()
 for c in classes:
-    print(c)
-    break
 
+    contents = c.b.contents
+
+    # 1-off
+    if (len(contents) > 1):
+        contents = [contents[-1]]
+
+    class_name = contents[0]
+    all_classes.add(class_name)
+
+    # if len(class_name) > 1:
+
+    # sloppy edge case
+    # if ()
+
+    # print(c.b.contents)
+    # break
+
+print(all_classes)
 print(len(classes))
 # for sib in html_soup.find('b').next_siblings:
 #     print(sib)
